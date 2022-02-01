91a61749eb3198960424a0a5ff9ce5f74332dae4
Matthew Russell <mrussell5361@gmail.com>
2022-02-01 15:55:23 -0500

    Stupid Fun Added To 01

---
+++ 01_Variables/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1 // 2 // 3 // 4
+Console.WriteLine("I will need to gather some quick information from you for research, what is your name?");
+
+string firstName = Console.ReadLine();
+Console.WriteLine(firstName);
+
+Console.WriteLine("So your name is " + firstName + "... Well it is nice to meet you!");
+
+Console.WriteLine("What is your favorite color " + firstName + "?");
+string color = Console.ReadLine();
+
+Console.WriteLine("You said that your favorite color is " + color + " HAHAHAHAHA... I am sorry I did't mean to laugh.");
