1a8b23b5ec4bb80015b321260d0a36384939c72b
Matthew Russell <mrussell5361@gmail.com>
2022-02-02 15:58:25 -0500

    added Programs to my Solutions/Project

---
+++ 02_Types_Challenges/Program.cs
@@ -0,0 +1,43 @@
+﻿int number;
+int numberOne = 1;
+
+string name;
+string longWord = "Supercalifragilistic";
+
+bool trueFalseValue;
+bool csharpIsFun = true;
+
+float averagePersonHeight;
+float height = 5.4f;
+
+double whatIsPie;
+double pie = 3.14d;
+
+decimal restaurantReceipt;
+decimal receipt = 48.58m;
+
+ulong positiveNumber;
+ulong maximumValue = 2853537682365566112;
+
+Console.WriteLine(numberOne);
+Console.WriteLine(csharpIsFun);
+
+
+string myName = "Matthew";
+int myAge = 25;
+int myHeight = 6;
+int kids = 1;
+
+Console.WriteLine("hello there my name is " + myName + " I am " + myAge + " years old, I am " + myHeight + " feet tall and I have " + kids + " kid.");
+
+string firstName = "Matthew";
+string lastName = "Russell";
+string myInformation = firstName + " " + lastName + " age " + myAge;
+Console.WriteLine(myInformation);
+
+string year = "2022";
+int yearNumber = int.Parse(year);
+Console.WriteLine(yearNumber);
+
+int anotherNumber = Convert.ToInt32(year);
+Console.WriteLine(anotherNumber);
---
+++ 04_Strings/Program.cs
@@ -0,0 +1,13 @@
+﻿string first = "The cars we sell are";
+string second = "BMW, Lexis, and Mercadies";
+string concatenatedResult = first + " " + second;
+Console.WriteLine(concatenatedResult);
+
+string firstName = "Jenn";
+string lastName = "williams";
+string compositeResult = string.Format("Her name is {0} {1}", firstName, lastName);
+Console.WriteLine(compositeResult);
+Console.WriteLine("Her name is {0} {1}", firstName, lastName);
+
+string interpolationResult = $"Her name is still {firstName} {lastName}!";
+Console.WriteLine(interpolationResult);
