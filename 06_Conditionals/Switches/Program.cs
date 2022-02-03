c6253b7e31623777b0b94129713c24b1506ecc53
Matthew Russell <mrussell5361@gmail.com>
2022-02-03 11:19:28 -0500

    Adding Switches to Fundamentals Solutions/Project

---
+++ 06_Conditionals/Switches/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Please give us your name!");
+string name = Console.ReadLine();
+switch(name)
+{
+    case "Matt":
+    Console.WriteLine("Nice to meet you " + name);
+    break;
+    default:
+    break;
+}
+
+
+Console.WriteLine("How are you feeling today on a scale of 1-5?");
+string feelingRating = Console.ReadLine();
+
+switch(feelingRating)
+{
+    case "5": 
+    Console.WriteLine("That is great to hear!");
+    break;
+    case "4":
+    Console.WriteLine("Good Stuff!");
+    break;
+    case "3":
+    Console.WriteLine("Hope things improve!");
+    break;
+    case "2":
+    Console.WriteLine("Oh no, sorry to hear that!");
+    break;
+    case "1":
+    Console.WriteLine("Dang hope everything gets better!");
+    break;
+}
