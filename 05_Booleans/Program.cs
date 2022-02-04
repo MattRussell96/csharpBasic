d9fccdcee511caba14b447f699d0e23c1ed7e456
Matthew Russell <mrussell5361@gmail.com>
2022-02-04 10:31:30 -0500

    Adding Booleans to Solution/Projects

--- 05_Booleans/Program.cs
+++ 05_Booleans/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isCool = false;
+isCool = true;
+
+bool isTrue = !false;
+bool isFalse = !true;
+
+bool trueAndExample = true && true;
+bool falseAndExample = true && false;
+
+bool trueOrExample = true || false;
+bool falseOrExample = false || true;
+
+// For exclusive or they have to be different no matter what for them to be true and vise a versa for false.//
+bool trueExclusiveOr = true ^ false;
+bool falseExclusiveOr = true ^ true;
+
+Console.WriteLine($"Not false = {isTrue}");
+Console.WriteLine($"False AND TRUE = {falseAndExample}");
+Console.WriteLine($"True OR True = {trueOrExample}");
+Console.WriteLine($"True XOR True = {falseExclusiveOr}");
+
+bool isGreaterThan = 10 > 10;
+bool isGreaterThanOrEqualTo = 10 >= 10;
+Console.WriteLine($"10 > 10 is {isGreaterThan}, and 10 >= 10 {isGreaterThanOrEqualTo}");
+
+bool isLessThan = 5 < 5;
+bool isLessThanOrEqualTo = 5 <= 5;
+Console.WriteLine($"5 < 5 is {isLessThan}, while 5 <= 5 {isLessThanOrEqualTo}");
+
+bool equality = 17 == 9;
+bool inequality = 17 != 9;
+Console.WriteLine($"17 == 9 and 17 != 9 are {equality} and {inequality}");
